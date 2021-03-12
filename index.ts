96675060c664affb0623e2fd3a4c6a458d8f7d43
Filiph Sandström <filiph.sandstrom@filfatstudios.com>
2021-03-12 16:28:18 +0100

    Bump version.

--- index.ts
+++ index.ts
@@ -1,69 +1,70 @@
 #!/usr/bin/env node
 /* eslint-disable import/no-extraneous-dependencies */
 
-import chalk from 'chalk';
-import Commander from 'commander';
-import packageJson from './package.json';
-import prompts from 'prompts';
-import { mkdirSync, writeFileSync } from 'fs';
-import path from 'path';
+import { mkdirSync, writeFileSync } from "fs";
 
-let projectPath: string = '';
+import Commander from "commander";
+import chalk from "chalk";
+import packageJson from "./package.json";
+import path from "path";
+import prompts from "prompts";
+
+let projectPath: string = "";
 const program = new Commander.Command(packageJson.name)
-    .version(packageJson.version)
-    .arguments('<project-directory>')
-    .usage(`${chalk.green('<project-directory>')} [options]`)
-    .action((name) => {
-        projectPath = name
-    })
-    .allowUnknownOption()
-    .parse(process.argv);
+  .version(packageJson.version)
+  .arguments("<project-directory>")
+  .usage(`${chalk.green("<project-directory>")} [options]`)
+  .action((name) => {
+    projectPath = name;
+  })
+  .allowUnknownOption()
+  .parse(process.argv);
 
 async function run(): Promise<void> {
-    if (typeof projectPath === 'string') {
-        projectPath = projectPath.trim();
-    }
+  if (typeof projectPath === "string") {
+    projectPath = projectPath.trim();
+  }
 
-    if (!projectPath) {
-        const res = await prompts({
-            type: 'text',
-            name: 'path',
-            message: 'What is your project npm name?',
-            initial: 'my-plugin',
-            validate: (name) => {
-                return true; // TODO: validate name
-            },
-        });
-
-        if (typeof res.path === 'string') {
-            projectPath = res.path.trim();
-        }
-    }
-
-    const { displayName } = await prompts({
-        type: 'text',
-        name: 'displayName',
-        message: 'What is your project display name?',
-        initial: 'My Plugin',
-        validate: (name) => {
-            return true; // TODO: validate name
-        },
+  if (!projectPath) {
+    const res = await prompts({
+      type: "text",
+      name: "path",
+      message: "What is your project npm name?",
+      initial: "my-plugin",
+      validate: (name: string) => {
+        return true; // TODO: validate name
+      },
     });
 
-    const { apiVersion } = await prompts({
-        type: 'text',
-        name: 'apiVersion',
-        message: 'What plugin api version does your project target?',
-        initial: '1.0',
-        validate: (name) => {
-            return true;
-        },
-    });
+    if (typeof res.path === "string") {
+      projectPath = res.path.trim();
+    }
+  }
 
-    mkdirSync(projectPath);
-    writeFileSync(
-      path.join(projectPath, "package.json"),
-      `{
+  const { displayName } = await prompts({
+    type: "text",
+    name: "displayName",
+    message: "What is your project display name?",
+    initial: "My Plugin",
+    validate: (name: string) => {
+      return true; // TODO: validate name
+    },
+  });
+
+  const { apiVersion } = await prompts({
+    type: "text",
+    name: "apiVersion",
+    message: "What plugin api version does your project target?",
+    initial: "1.0",
+    validate: (name: string) => {
+      return true;
+    },
+  });
+
+  mkdirSync(projectPath);
+  writeFileSync(
+    path.join(projectPath, "package.json"),
+    `{
     "name": "${projectPath}",
     "version": "1.0.0",
     "description": "",
@@ -91,12 +92,17 @@ async function run(): Promise<void> {
         "renamer": "2.0.1"
     }
 }`
-    );
-    writeFileSync(path.join(projectPath, '.gitignore'), `/node_modules/
+  );
+  writeFileSync(
+    path.join(projectPath, ".gitignore"),
+    `/node_modules/
 /dist/
 *.log
-.DS_Store`);
-    writeFileSync(path.join(projectPath, 'index.ts'), `import type PluginApi from '@jsprismarine/prismarine/dist/plugin/api/versions/${apiVersion}/PluginApi';
+.DS_Store`
+  );
+  writeFileSync(
+    path.join(projectPath, "index.ts"),
+    `import type PluginApi from '@jsprismarine/prismarine/dist/plugin/api/versions/${apiVersion}/PluginApi';
 
 export default class PluginBase {
     api: PluginApi;
@@ -107,8 +113,11 @@ export default class PluginBase {
 
     public async onEnable() { }
     public async onDisable() { }
-}`);
-    writeFileSync(path.join(projectPath, 'tsconfig.json'), `{
+}`
+  );
+  writeFileSync(
+    path.join(projectPath, "tsconfig.json"),
+    `{
     "compilerOptions": {
         "target": "es2015",
         "moduleResolution": "node",
@@ -117,8 +126,11 @@ export default class PluginBase {
         "esModuleInterop": true,
         "skipLibCheck": false
     }
-}`);
-    writeFileSync(path.join(projectPath, 'README.md'), `# create-jsprismarine-plugin
+}`
+  );
+  writeFileSync(
+    path.join(projectPath, "README.md"),
+    `# create-jsprismarine-plugin
 ## Usage
 \`\`\`bash
 # Build and minify your source code
@@ -129,17 +141,17 @@ $ npm run archive
 \`\`\`
 
 ${projectPath}.jspz is the resulting file that should be placed in the \`/plugins\` folder!
-`);
+`
+  );
 }
 
-run()
-    .catch(async (reason) => {
-        console.log('Aborting installation.')
-        if (reason.command) {
-            console.log(`  ${chalk.cyan(reason.command)} has failed.`)
-        } else {
-            console.log(chalk.red('Unexpected error. Please report it as a bug:'))
-            console.log(reason)
-        }
-        process.exit(1)
-    });
+run().catch(async (reason) => {
+  console.log("Aborting installation.");
+  if (reason.command) {
+    console.log(`  ${chalk.cyan(reason.command)} has failed.`);
+  } else {
+    console.log(chalk.red("Unexpected error. Please report it as a bug:"));
+    console.log(reason);
+  }
+  process.exit(1);
+});
